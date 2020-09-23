362095742dcb0f53c070518fa741265fe1d3dc04
lompo <lompo@iuj.ac.jp>
2020-09-23 18:43:00 +0900

    added edit fonction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,29 @@
 class UsersController < ApplicationController
-def new
-  @user = User.new
-  if logged_in?
-    redirect_to feeds_path
-end
-end
+  before_action :authenticate_user, only: [:show]
 
-def create
-   @user = User.new(user_params)
-   if @user.save
-     redirect_to user_path(@user.id)
-   else
-     render :new
-   end
- end
+  def new
+    @user = User.new
+    if logged_in?
+      redirect_to feeds_path
+    end
+  end
 
- def show
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+  def show
     @user = User.find(params[:id])
   end
 
-  def update
-    if @feed.update(blog_params)
-      redirect_to feed_path, notice: "feed was successfully updated！"
-    else
-      render :edit
-    end
-  end
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- db/migrate/20200915122747_add_image_to_feed.rb
+++ db/migrate/20200915122747_add_image_to_feed.rb
@@ -2,6 +2,7 @@ class AddImageToFeed < ActiveRecord::Migration[5.2]
   def change
     add_column :feeds, :title, :string
     add_column :feeds, :content, :string
+    add_column :feeds, :username, :string
   end
 
 end
